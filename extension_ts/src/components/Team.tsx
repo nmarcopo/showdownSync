32e796d3d9497c072454b70df1c3547aad559d4e
Nick Marcopoli <nick@marcopo.li>
2022-03-28 20:12:08 -0400

    Basic team name functionality

--- extension_ts/src/components/Team.tsx
+++ extension_ts/src/components/Team.tsx
@@ -1,13 +1,30 @@
 import React from "react";
 
-import { Card } from "@blueprintjs/core";
+import { Card, H4, H6 } from "@blueprintjs/core";
 
-export class Team extends React.Component <{}> {
+enum Status {
+    NOT_BACKED_UP,
+    BACKED_UP_AND_AVAILABLE,
+    BACKED_UP_AND_NOT_AVAILABLE,
+}
+
+export interface TeamProps {
+    team_format: string;
+    team_name: string;
+}
+
+interface TeamState {
+    team_composition: string;
+    team_status: Status;
+}
+
+export class Team extends React.Component<TeamProps> {
     render() {
         return (
             <div>
                 <Card>
-                    Hello world!
+                    <H4>Team: {this.props.team_name}</H4>
+                    <H6>Format: {this.props.team_format}</H6>
                 </Card>
             </div>
         );
--- extension_ts/src/react-popup/App.tsx
+++ extension_ts/src/react-popup/App.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { Card } from "@blueprintjs/core";
 
 import { Title } from "./Title"
+import { Team } from "../components/Team"
 
 export class App extends React.Component {
     render() {
         return (
-            <Title />
+            <div>
+                <Title />
+                <Team team_name="My New Team" team_format="gen8" />
+            </div>
         );
     };
 }
