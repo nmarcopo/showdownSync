fd005fcf89f12337ccd39ff9cde2a9455dfa9270
Nick Marcopoli <nick@marcopo.li>
2022-04-25 14:10:44 -0400

    Add latest changes

--- extension_ts/src/background.ts
+++ extension_ts/src/background.ts
@@ -1,1 +1,23 @@
-// TODO: Fill this out
+export async function getCurrentTab() {
+    let queryOptions = { active: true, currentWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;  
+}
+
+chrome.runtime.onMessage.addListener(function sender(request, sender, sendResponse) {
+    switch (request.type) {
+        case "getLocalStorage": {
+            console.log(localStorage)
+            sendResponse({
+                data: "this is a response!"
+            });
+            // sendResponse({
+                // storage = 
+        }
+        default: {
+            console.log("Unknown message type: " + request.type);
+            console.log(request);
+            console.log(chrome.storage.local.get("teams"));
+        }
+    }
+});
--- extension_ts/src/components/TeamList.tsx
+++ extension_ts/src/components/TeamList.tsx
@@ -3,6 +3,9 @@ import React from "react";
 import { Card, H4, H6 } from "@blueprintjs/core";
 import { Team } from "./Team";
 
+import { getCurrentTab } from "../background";
+// import { returnValues } from "../scripts/injection";
+
 export enum TeamListType {
     AVAILABLE_TEAMS = "Available Teams",
     RESTORE_TEAMS = "Restore Teams"
@@ -18,11 +21,72 @@ export interface TeamListProps {
 // }
 
 export class TeamList extends React.Component<TeamListProps> {
+    componentDidMount() {
+        this.getTeams();
+    }
+
+    injectionScript() {
+        console.log("injection script");
+        // console.log(window.localStorage);
+        // chrome.runtime.sendMessage({
+        //     type: "getLocalStorage",
+        // });
+        console.log("window.localStorage: ", window.localStorage);
+        return window.localStorage;
+    }
+
+    getTeams() {
+        return getCurrentTab().then((tab) => {
+            if (tab.id) {
+                chrome.scripting.executeScript({
+                    target: { tabId: tab.id },
+                    func: () => localStorage.getItem("showdown_teams"),
+                    // func: () => (window as any).room.update(),
+                    // For the restore script: https://stackoverflow.com/a/30740935
+                }, (results) => {
+                    // results = results[0];
+                    console.log("results: ", results.at(0)?.result);
+                    return results.at(0)?.result;
+                });
+            } else {
+                console.error("Error: No tab id. Tab: ", tab)
+                return ""
+            }
+        }).catch((err) => {
+            console.error(err)
+            return ""
+        });
+    }
+
+    _getTeamsHelper(teams: string) {
+
+    }
+
+    Teams = async (props: TeamListProps) => {
+        switch (props.team_list_type) {
+            case (TeamListType.AVAILABLE_TEAMS): {
+                let teams: string | undefined = await this.getTeams();
+                if (teams !== undefined) {
+                    return this._getTeamsHelper(teams);
+                }
+            }
+            case (TeamListType.RESTORE_TEAMS): {
+
+            }
+            default: {
+                console.error("Error: team list type is:", props.team_list_type)
+            }
+        }
+    }
+
     render() {
         return (
             <div className="docs-example">
+                {/* {this.getTeams()} */}
                 {/* <H6>{this.props.team_list_type}</H6> */}
-                <Team team_name={this.props.team_list_type + " test team"} team_format="gen8" />
+
+                <Team team_name={this.props.team_list_type + " test team"} team_format="gen8"
+                />
                 <Team team_name={this.props.team_list_type + " test team2"} team_format="gen8" />
             </div>
         );
