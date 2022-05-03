ab1c0c049e3f1277156ae090a4137893403d8894
Nick Marcopoli <nick@marcopo.li>
2022-05-02 23:26:50 -0400

    Grab iconCache via window.Storage if necessary

--- extension_ts/src/components/Team.tsx
+++ extension_ts/src/components/Team.tsx
@@ -6,6 +6,7 @@ enum Status {
     NOT_BACKED_UP,
     BACKED_UP_AND_AVAILABLE,
     BACKED_UP_AND_NOT_AVAILABLE,
+    CURRENTLY_BEING_EDITED,
 }
 
 export interface TeamProps {
@@ -16,6 +17,7 @@ interface TeamState {
     team_composition: string;
     team_status: Status;
     interactive: boolean;
+    iconCache: string;
 }
 
 export interface ShowdownTeamJson {
@@ -32,18 +34,42 @@ export class Team extends React.Component<TeamProps, TeamState> {
         team_composition: "bulba, charmander",
         team_status: Status.NOT_BACKED_UP,
         interactive: true,
+        iconCache: "lorem ipsum",
         // Iconcache should probably be it's own state here so the component can get updated when iconcache is loaded
     }
 
+    componentDidMount() {
+        this.getIcons(this.props.team).then((response) => {
+            this.setState({
+                iconCache: response
+            });
+        });
+    }
+
     async getIcons(team: ShowdownTeamJson) {
-        const [{ result }] = await chrome.scripting.executeScript({
-            func: () => (window.Storage as any).getTeamIcons(team),
+        console.log("team", JSON.stringify(team));
+        // IconCache can be an exclamation point
+        // Return iconcache if it's already defined in props
+        if (this.props.team.iconCache.length > 1) {
+            console.log("iconcache is defined in props", this.props.team.iconCache);
+            return this.props.team.iconCache;
+        }
+        if (this.props.team.iconCache === "!") {
+            console.log("Team is being edited")
+            this.setState({
+                team_status: Status.CURRENTLY_BEING_EDITED,
+            });
+        }
+        const [{result}] = await chrome.scripting.executeScript({
+            func: (team) => (window.Storage as any).getTeamIcons(team),
+            args: [team],
             target: {
                 tabId:
                     (await chrome.tabs.query({ active: true, currentWindow: true }))[0].id!
             },
             world: 'MAIN',
         });
+        console.log("iconcache lookup:", result);
         return result;
     }
 
@@ -56,7 +82,8 @@ export class Team extends React.Component<TeamProps, TeamState> {
                         <Tag className="team-format-tag">{this.props.team.format}</Tag>
                         {this.props.team.name}
                     </small>
-                    <span dangerouslySetInnerHTML={{ __html: this.props.team.iconCache }} />
+                    {/* DangerouslySetInnerHTML is OK here since we know that this will be sent via iconCache, so no risk of XSS */}
+                    <span dangerouslySetInnerHTML={{ __html: this.state.iconCache }} />
                     <ButtonGroup>
                         <Button>Backup</Button>
                     </ButtonGroup>
